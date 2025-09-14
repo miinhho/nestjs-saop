02c063463742f8129c77d6cbac8aae535b86fe0b
minho jang <mjang6988@gmail.com>
2025-09-14 23:30:10 +0900

    chore: edit lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ import micromatch from 'micromatch';
 
 export default {
   '*.{ts,js}': stagedFiles => {
-    const match = micromatch.not(stagedFiles, 'node_modules/**');
+    const match = micromatch.not(stagedFiles, 'node_modules/**/*');
     return [`eslint --fix ${match.join(' ')}`, `prettier --write ${match.join(' ')}`];
   },
 };
